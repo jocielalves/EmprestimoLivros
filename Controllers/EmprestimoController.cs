35d2dcd9443d78d290d4bc50c46c2a591629b6f1
jociel <jocielalvesdejesus@gmail.com>
2023-02-22 17:21:35 -0300

    projeto emprestimo mvc, apenas recuperação de emprestimos implementado

---
+++ Controllers/EmprestimoController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using EmprestimoLivros.Data;
+using EmprestimoLivros.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmprestimoLivros.Controllers
+{
+    public class EmprestimoController : Controller
+    {
+        readonly private EmprestimoContext _dbContext;
+        public EmprestimoController(EmprestimoContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<EmprestimosModel> emprestimos = _dbContext.Emprestimos;
+            return View(emprestimos);
+        }
+    }
+}
---
+++ Data/EmprestimoContext.cs
@@ -0,0 +1,15 @@
+﻿using EmprestimoLivros.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmprestimoLivros.Data
+{
+    public class EmprestimoContext : DbContext
+    {
+        public EmprestimoContext(DbContextOptions<EmprestimoContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<EmprestimosModel> Emprestimos { get; set;}
+    }
+}
